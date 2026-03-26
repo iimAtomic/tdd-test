31b20fbdcca040a93da78d8f5316ee310fb623e1
iimAtomic <vegbahermerite@gmail.com>
2026-03-26 16:53:27 +0100

    feat: error pair detection

--- src/evaluator/evaluateHand.ts
+++ src/evaluator/evaluateHand.ts
@@ -1,6 +1,16 @@
-import { type Card } from "../models/Card.js";
-import { HandRank } from "../models/HandRank.js";
+import { Card } from "../models/Card";
+import { HandRank } from "../models/HandRank";
 
 export function evaluateHand(cards: Card[]): [HandRank, number[]] {
+
+  // 👇 CODE NAÏF VOLONTAIRE
+  for (let i = 0; i < cards.length; i++) {
+    for (let j = i + 1; j < cards.length; j++) {
+      if (cards[i].rank === cards[j].rank) {
+        return [HandRank.PAIR, []];
+      }
+    }
+  }
+
   return [HandRank.HIGH_CARD, []];
 }
